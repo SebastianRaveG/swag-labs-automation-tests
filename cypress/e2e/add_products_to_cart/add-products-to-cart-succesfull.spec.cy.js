bc16ae0b9bdce4f62e4db7a110ab63c398990cdd
Laura Sofia <lsospina29@gmail.com>
2024-08-17 14:04:38 -0500

    Actualización

--- cypress/e2e/add_products_to_cart/add-products-to-cart-succesfull.spec.cy.js
+++ cypress/e2e/add_products_to_cart/add-products-to-cart-succesfull.spec.cy.js
@@ -10,8 +10,8 @@ describe('Add Products to Cart - Succesfull', () => {
     cy.get('#add-to-cart-sauce-labs-bike-light').click()
     cy.get('.shopping_cart_link').click()
 
-    cy.get('.cart_item').should('have.length', 2);
-    cy.get('.inventory_item_name').first().should('contain.text', 'Sauce Labs Backpack');
-    cy.get('.inventory_item_name').last().should('contain.text', 'Sauce Labs Bike Light');
+    cy.get('.cart_item').should('have.length', 2)
+    cy.get('.inventory_item_name').first().should('contain.text', 'Sauce Labs Backpack')
+    cy.get('.inventory_item_name').last().should('contain.text', 'Sauce Labs Bike Light')
   })
 })
