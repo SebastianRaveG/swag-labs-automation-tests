fc885dd73b2cc0bb456a6b11c70a18eefbfb9246
Laura Sofia <lsospina29@gmail.com>
2024-08-17 11:18:19 -0500

    Logout test

---
+++ cypress/e2e/Add Products to Cart/AddProductsToCart-succesfull.spec.cy.js
@@ -0,0 +1,13 @@
+describe('Add Products to Cart - Succesfull', () => {
+  beforeEach(() => {
+    cy.fixture('users').then((users) => {
+      cy.clearAllAndLogin(users.validUser.username, users.validUser.password)
+    })
+  })
+
+  it('Should show an error message with blocked credentials', () => {
+    cy.get('#add-to-cart-sauce-labs-backpack').click()
+    cy.get('#add-to-cart-sauce-labs-bike-light').click()
+    cy.get('.shopping_cart_link').click()
+  })
+})
--- cypress/e2e/Logout/logout-succesfull.spec.cy.js
+++ cypress/e2e/Logout/logout-succesfull.spec.cy.js
@@ -3,14 +3,12 @@ describe('Logout Functionality - Success Cases', () => {
     // Cargar datos de usuario válido
     cy.fixture('users').then((users) => {
       cy.clearAllAndLogin(users.validUser.username, users.validUser.password)
-      cy.get('#react-burger-menu-btn').click()
-      cy.get('#logout_sidebar_link').click()
-
     })
   })
 
   it('Should log in with valid credentials', () => {
-    cy.url().should('include', '')
-  })
-
+      cy.get('#react-burger-menu-btn').click()
+      cy.get('#logout_sidebar_link').click()
+      cy.url().should('include', '')
+   })
 })
