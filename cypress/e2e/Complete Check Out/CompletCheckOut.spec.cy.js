b3892045e933aed0eea9dd2a543d4113b684b776
SebasrtianRaveG <seba339931@gmail.com>
2024-08-28 08:52:12 -0500

    Add the case for complete checkout succesfull

--- cypress/e2e/Complete Check Out/CompletCheckOut.spec.cy.js
+++ cypress/e2e/Complete Check Out/CompletCheckOut.spec.cy.js
@@ -1,17 +1,36 @@
-describe('Sort Products by Name A to Z - Succesfull', () => {
-   beforeEach(() => {
+describe('Login Functionality - Success Cases', () => {
+  beforeEach(() => {
     cy.fixture('users').then((users) => {
       cy.clearAllAndLogin(users.validUser.username, users.validUser.password)
     })
   })
 
-  it('Should add products to the cart and verify them', () => {
-    cy.get('.product_sort_container').select('Name (A to Z)')
+  it('Complete Check out', () => {
+    cy.get('#add-to-cart-sauce-labs-backpack').click()
+    cy.get('#add-to-cart-sauce-labs-bike-light').click()
+    cy.get('.shopping_cart_link').click()
 
-    cy.get('.inventory_item_name').then(($names) => {
-      const names = $names.map((index, html) => Cypress.$(html).text()).get();
-      const sortedNames = [...names].sort();
-      expect(names).to.deep.equal(sortedNames);
-    });
+    cy.get('.cart_item').should('have.length', 2)
+    cy.get('.inventory_item_name').first().should('contain.text', 'Sauce Labs Backpack')
+    cy.get('.inventory_item_name').last().should('contain.text', 'Sauce Labs Bike Light')
+    cy.get('#checkout').click()
+    
+    cy.get('.title').should('contain.text', 'Checkout: Your Information')
+    
+    cy.get('#first-name').type('Usurio')
+    cy.get('#last-name').type('Pruebas')
+    cy.get('#postal-code').type(17001)
+    cy.get('#continue').click()
+    
+    cy.get('.title').should('contain.text', 'Checkout: Overview')
+    cy.get('.cart_item').should('have.length', 2)
+    cy.get('.inventory_item_name').first().should('contain.text', 'Sauce Labs Backpack')
+    cy.get('.inventory_item_name').last().should('contain.text', 'Sauce Labs Bike Light')
+    
+    cy.get('#finish').click()
+    cy.get('.title').should('contain.text', 'Checkout: Complete!')
+    
+    cy.get('#back-to-products').click()
+    cy.url().should('include', '/inventory.html')
   })
 })
