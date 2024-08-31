989f477b280e782488f6327d4121b84f1a718734
SebasrtianRaveG <seba339931@gmail.com>
2024-08-31 14:30:01 -0500

    Add the validation for logout test

--- cypress/e2e/logout/logout_succesfull.spec.cy.js
+++ cypress/e2e/logout/logout_succesfull.spec.cy.js
@@ -1,14 +1,31 @@
-describe('Logout Functionality - Success Cases', () => {
-  beforeEach(() => {
-    // Cargar datos de usuario válido
-    cy.fixture('users').then((users) => {
-      cy.clearAllAndLogin(users.validUser.username, users.validUser.password)
-    })
-  })
+const viewports = Cypress.config('viewports');
+let users;
 
-  it('Should log in with valid credentials', () => {
-      cy.get('#react-burger-menu-btn').click()
-      cy.get('#logout_sidebar_link').click()
-      cy.url().should('include', '')
-   })
-})
+beforeEach(() => {
+  cy.clearAll();
+  cy.fixture('users').then((fixtureData) => {
+    users = fixtureData;
+  });
+});
+
+describe('Logout Functionality - Succesfull Cases', () => {
+
+  viewports.forEach((size) => {
+    Cypress._.each(['validUsers'], (userType) => {
+
+      it(`Should logout logged valid users in ${userType} on ${size} screen`, function () {
+        cy.setViewport(size);
+        users[userType].forEach((user) => {
+          cy.clearAllAndLogin(user.username, user.password);
+          cy.url().should('include', '/inventory.html');
+
+          cy.get('.bm-burger-button').click();
+          cy.get('#logout_sidebar_link').click();
+          cy.log(`Testing logout for user: ${user.username}`);
+          cy.url().should('include', '')
+
+        });
+      });
+    });
+  });
+});
