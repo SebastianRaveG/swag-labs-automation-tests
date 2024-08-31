ac7401597bf2fae262ed300336057697f8b2ed67
SebasrtianRaveG <seba339931@gmail.com>
2024-08-31 14:13:47 -0500

    Fix login empty

--- cypress/e2e/login/login_empty.spec.cy.js
+++ cypress/e2e/login/login_empty.spec.cy.js
@@ -12,7 +12,7 @@ describe('Login Functionality - Empty Cases', () => {
     viewports.forEach((size) => {
         it(`Should show an error message with Username is required on ${size} screen`, () => {
             cy.setViewport(size);
-            cy.setViewport(size);
+            cy.visit('https://www.saucedemo.com/')
             cy.get('#login-button').click()
             cy.get('.error-message-container').should('be.visible');
         })
